a6b234b2fe91a525abb6c049f7bb5c01ebadcfa4
senfan <senfan235@163.com>
2024-01-10 23:13:24 +0800

    基本页面编写

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <BasicLayout />
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
+
+onMounted(() => {
+  doInit();
+});
+</script>
---
+++ src/views/question/QuestionsView.vue
@@ -0,0 +1,175 @@
+<template>
+  <div id="questionsView">
+    <a-form :model="searchParams" layout="inline">
+      <a-form-item field="title" label="名称" style="min-width: 240px">
+        <a-input v-model="searchParams.title" placeholder="请输入名称" />
+      </a-form-item>
+      <a-form-item field="tags" label="标签" style="min-width: 240px">
+        <a-input-tag v-model="searchParams.tags" placeholder="请输入标签" />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" @click="doSubmit">提交</a-button>
+      </a-form-item>
+    </a-form>
+    <a-divider size="0" />
+    <a-table
+      :columns="columns"
+      :data="dataList"
+      :pagination="{
+        showTotal: true,
+        pageSize: searchParams.pageSize,
+        current: searchParams.current,
+        total,
+      }"
+      @page-change="onPageChange"
+    >
+      <template #tags="{ record }">
+        <a-space wrap>
+          <a-tag v-for="(tag, index) of record.tags" :key="index" color="green"
+            >{{ tag }}
+          </a-tag>
+        </a-space>
+      </template>
+      <template #acceptedRate="{ record }">
+        {{
+          `${
+            record.submitNum ? record.acceptedNum / record.submitNum : "0"
+          }% (${record.acceptedNum}/${record.submitNum})`
+        }}
+      </template>
+      <template #createTime="{ record }">
+        {{ moment(record.createTime).format("YYYY-MM-DD") }}
+      </template>
+      <template #optional="{ record }">
+        <a-space>
+          <a-button type="primary" @click="toQuestionPage(record)">
+            做题
+          </a-button>
+        </a-space>
+      </template>
+    </a-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watchEffect } from "vue";
+import { useRouter } from "vue-router";
+import message from "@arco-design/web-vue/es/message";
+import { Question } from "../../../generated/models/Question";
+import {
+  QuestionControllerService,
+  QuestionQueryRequest,
+} from "../../../generated";
+import moment from "moment";
+
+const dataList = ref([]);
+const total = ref(0);
+const searchParams = ref<QuestionQueryRequest>({
+  title: "",
+  tags: [],
+  pageSize: 8,
+  current: 1,
+});
+
+const loadData = async () => {
+  const res = await QuestionControllerService.listMyQuestionVoByPageUsingPost(
+    searchParams.value
+  );
+  if (res.code === 0) {
+    dataList.value = res.data.records;
+    total.value = res.data.total;
+  } else {
+    message.error("加载失败，" + res.message);
+  }
+};
+
+/**
+ * 监听 searchParams 变量，改变时触发页面的重新加载
+ */
+watchEffect(() => {
+  loadData();
+});
+
+/**
+ * 页面加载时，请求数据
+ */
+onMounted(() => {
+  loadData();
+});
+
+// {id: "1", title: "A+ D", content: "新的题目内容", tags: "["二叉树"]", answer: "新的答案", submitNum: 0,…}
+
+const columns = [
+  {
+    title: "题号",
+    dataIndex: "id",
+  },
+  {
+    title: "题目名称",
+    dataIndex: "title",
+  },
+  {
+    title: "标签",
+    slotName: "tags",
+  },
+  {
+    title: "通过率",
+    slotName: "acceptedRate",
+  },
+  {
+    title: "创建时间",
+    slotName: "createTime",
+  },
+  {
+    slotName: "optional",
+  },
+];
+
+const onPageChange = (page: number) => {
+  searchParams.value = {
+    ...searchParams.value,
+    current: page,
+  };
+};
+const doDelete = async (question: Question) => {
+  const res = await QuestionControllerService.deleteQuestionUsingPost({
+    id: question.id,
+  });
+  if (res.code === 0) {
+    message.success("删除成功");
+    await loadData();
+  } else {
+    message.error("删除失败");
+  }
+};
+
+const router = useRouter();
+
+/**
+ * 跳转到做题页面
+ * @param question
+ */
+const toQuestionPage = (question: Question) => {
+  router.push({
+    path: `/view/question/${question.id}`,
+  });
+};
+
+/**
+ * 确认搜索，重新加载数据
+ */
+const doSubmit = () => {
+  // 这里需要重置搜索页号
+  searchParams.value = {
+    ...searchParams.value,
+    current: 1,
+  };
+};
+</script>
+
+<style scoped>
+#questionsView {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
